f0babf8cede8a1da133baad3a779aa380ba965b1
hk <kiptooharon.hk@gmail.com>
2024-08-27 17:00:36 +0300

    create table if not exsts

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,6 +1,8 @@
 -- create users table
+-- if not exist flasg
 CREATE TABLE IF NOT EXISTS users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL,
     name VARCHAR(255)
 );
+
