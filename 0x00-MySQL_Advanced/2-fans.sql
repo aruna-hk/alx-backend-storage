38fab7e88ec69f85e47a12736b68565a0e1e03c8
hk <kiptooharon.hk@gmail.com>
2024-08-27 17:30:52 +0300

    group by &order by

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,3 +1,7 @@
 -- list bands
 -- in order
-SELECT origin, fans AS nb_fans FROM metal_bands ORDER BY nb_fans DESC; 
+SELECT origin, SUM(fans) AS nb_fans
+FROM metal_bands
+GROUP BY origin
+ORDER BY nb_fans DESC;
+
