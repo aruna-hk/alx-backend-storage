52344fa59d4383bd7a7d628acaaf31b1a3508cea
hk <kiptooharon.hk@gmail.com>
2024-08-27 17:03:57 +0300

    unique constrain update

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,7 +1,7 @@
 -- create users table
 CREATE TABLE IF NOT EXISTS users (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    email VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255),
     country ENUM("US", "CO", "TN") DEFAULT "US"
 );
